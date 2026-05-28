1d4f42e52a051f6a9814291256276351f9c24b85
Abril <abril.bugna@mi.unc.edu.ar>
2026-05-28 05:40:48 +0000

    Add jazzmin admin theme

--- misso/settings.py
+++ misso/settings.py
@@ -14,6 +14,8 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ["misso-ecommerce.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
+    'jazzmin',
+    'django.contrib.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
