43f2a553d52a2d4dd0d1f6c695110cab5f77fb7d
eipril <164792121+abrilbugna@users.noreply.github.com>
2026-06-22 21:23:21 -0300

    add allowed hosts

--- misso/settings.py
+++ misso/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["misso-ecommerce.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["misso-ecommerce.onrender.com", "localhost", "127.0.0.1", "misso.ar", "www.misso.ar"]
 
 INSTALLED_APPS = [
     'jazzmin',
